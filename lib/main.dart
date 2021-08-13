eba8b06bcf99fc3f0cd15532b51897260fc9f77c
Bikash@888 <bikashdulal150@gmail.com>
2021-08-13 14:49:15 +0545

    folder restructured and routing added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_basic/home_page.dart';
+import 'package:flutter_basic/pages/home_page.dart';
+import 'package:flutter_basic/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,8 +11,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    checkUser(username: "bikash.dulal");
     return MaterialApp(
-      home: Home(),
+      home: LoginPage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/login": (context) => LoginPage(),
+      },
     );
   }
+}
+
+checkUser({@required username, password = ""}) {
+  // do something
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+          child: Text(
+        "Welcome to Login page",
+        style: TextStyle(
+          fontSize: 30,
+          color: Colors.blue,
+          fontWeight: FontWeight.bold,
+        ),
+      )),
+    );
+  }
+}
