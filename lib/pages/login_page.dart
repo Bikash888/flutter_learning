c08b4cb0e1b5aaf9610019d4e1bd42817431d224
Bikash@888 <bikashdulal150@gmail.com>
2021-08-18 19:22:25 +0545

    added loading icons in button

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_basic/utils/route.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = '';
+  bool loading = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -11,11 +18,11 @@ class LoginPage extends StatelessWidget {
           children: [
             SizedBox(height: 50),
             Image.asset("assets/images/login_image.jpg",
-                height: 280, fit: BoxFit.cover),
+                height: 190, fit: BoxFit.cover),
             SizedBox(
               height: 10,
             ),
-            Text("Grantha",
+            Text("Grantha $name",
                 style: TextStyle(
                   fontSize: 52,
                   fontWeight: FontWeight.w900,
@@ -25,6 +32,10 @@ class LoginPage extends StatelessWidget {
               child: Column(
                 children: [
                   TextFormField(
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                     decoration: InputDecoration(
                         labelStyle: TextStyle(
                             color: Colors.black45,
@@ -33,7 +44,7 @@ class LoginPage extends StatelessWidget {
                         labelText: "Email address",
                         hintText: "Enter email address"),
                   ),
-                  SizedBox(height: 18),
+                  SizedBox(height: 25),
                   TextFormField(
                     obscureText: true,
                     decoration: InputDecoration(
@@ -44,17 +55,47 @@ class LoginPage extends StatelessWidget {
                         labelText: "Password",
                         hintText: "Enter password"),
                   ),
-                  SizedBox(height: 18),
-                  ElevatedButton(
-                    onPressed: () =>
-                        {Navigator.pushNamed(context, GranthaRoutes.HomeRoute)},
-                    style: TextButton.styleFrom(
-                        backgroundColor: Colors.teal[400],
-                        minimumSize: Size(150, 40)),
-                    child: Text("Login",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w500, fontSize: 22)),
+                  SizedBox(height: 25),
+                  InkWell(
+                    onTap: () => {
+                      // Navigator.pushNamed(context, GranthaRoutes.HomeRoute)
+                      setState(() {
+                        loading = true;
+                      })
+                    },
+                    child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: loading ? 80 : 150,
+                        height: 50,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.teal[400],
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: loading
+                            ? Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 8, horizontal: 2),
+                                child: CircularProgressIndicator(
+                                  color: Colors.white,
+                                ),
+                              )
+                            : Text("Login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 22))),
                   )
+                  // ElevatedButton(
+                  //   onPressed: () =>
+                  //       {Navigator.pushNamed(context, GranthaRoutes.HomeRoute)},
+                  //   style: TextButton.styleFrom(
+                  //       backgroundColor: Colors.teal[400],
+                  //       minimumSize: Size(150, 40)),
+                  //   child: Text("Login",
+                  //       style: TextStyle(
+                  //           fontWeight: FontWeight.w500, fontSize: 22)),
+                  // )
                 ],
               ),
             )
