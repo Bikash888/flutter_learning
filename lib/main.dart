0ca5fdbc127d4b75280eb6392b441608dc2d4de8
Bikash@888 <bikashdulal150@gmail.com>
2021-09-01 13:36:47 +0545

    firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_basic/pages/cart_page.dart';
 import 'package:flutter_basic/pages/home_page.dart';
@@ -5,7 +6,9 @@ import 'package:flutter_basic/pages/login_page.dart';
 import 'package:flutter_basic/utils/route.dart';
 import 'package:flutter_basic/widgets/themes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
